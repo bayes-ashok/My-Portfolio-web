752d21b28447e4eb64bdaa2fe43e7f1472efddb9
Ashok <bayes.ashok@gmail.com>
2023-10-03 21:27:14 +0545

    adding email feature

--- forms/contact.php
+++ forms/contact.php
@@ -1,24 +1,19 @@
 <?php
-  
-  $receiving_email_address = 'bayes.ashok@gmail.com';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+    $to = "bayes.ashok@gmail.com"; 
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Your message has been sent. Thank you!";
+    } else {
+        echo "Sorry, there was an error sending your message.";
+    }
+}
 ?>
